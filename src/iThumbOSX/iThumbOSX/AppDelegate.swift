5087d583f762e1dd4d3aaa04bb1afba510e1ea5d
Jeffrey Lee Li <Jeffrey.Lee.Li@gmail.com>
2015-10-20 05:34:13 +0800

    add OS X app

---
+++ src/iThumbOSX/iThumbOSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iThumbOSX
+//
+//  Created by Lizhai Li on 10/18/15.
+//  Copyright © 2015 J&L. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ src/iThumbOSX/iThumbOSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iThumbOSX
+//
+//  Created by Lizhai Li on 10/18/15.
+//  Copyright © 2015 J&L. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
